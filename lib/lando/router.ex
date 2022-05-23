ca8590e1d060a685e2e1bc4cef88af9fb928c29d
Daniel Grieve <dnlgrv@hey.com>
2022-05-23 10:46:03 +0100

    Render template listing connected nodes

--- lib/lando/router.ex
+++ lib/lando/router.ex
@@ -4,7 +4,14 @@ defmodule Lando.Router do
   plug(:match)
   plug(:dispatch)
 
+  @template EEx.compile_file(Path.join([File.cwd!(), "lib/lando/templates/index.html.eex"]))
+
   get "/" do
-    send_resp(conn, 200, "Hello")
+    {result, _bindings} = Code.eval_quoted(@template)
+    send_resp(conn, 200, result)
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not found")
   end
 end
