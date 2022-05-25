03485a83ed6d0034501c4b2ab4ce459d9b852a9c
Daniel Grieve <dnlgrv@hey.com>
2022-05-25 20:19:09 +0100

    Only use DNS libcluster strategy in prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,12 +3,14 @@ import Config
 config :lando, Lando.Endpoint, port: System.get_env("PORT")
 config :lando, :secret, System.get_env("SECRET", "Not set")
 
-config :lando, :topologies,
-  ecs: [
-    strategy: Cluster.Strategy.DNSPoll,
-    config: [
-      polling_interval: 1000,
-      query: System.get_env("SERVICE_DISCOVERY_ENDPOINT"),
-      node_basename: System.get_env("NODE_NAME_QUERY")
+if config_env() == :prod do
+  config :lando, :topologies,
+    ecs: [
+      strategy: Cluster.Strategy.DNSPoll,
+      config: [
+        polling_interval: 1000,
+        query: System.get_env("SERVICE_DISCOVERY_ENDPOINT"),
+        node_basename: System.get_env("NODE_NAME_QUERY")
+      ]
     ]
-  ]
+end
